bfc172d5958b734d3ae4e24b9934eb2b10f5ac6b
prasad-hp <itsprasadhp@gmail.com>
2023-12-15 08:09:06 +0530

    Added the dark and light mode

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-function mode() {
-    document.getElementById(color-change)
-}
+
+    let icon = document.getElementById("icon");
+
+    icon.onclick = function() {
+        document.body.classList.toggle("dark-theme");
+        if(document.body.classList.contains("dark-theme")) {
+            icon.src = "sun.png"
+        }else{
+            icon.src = "moon.png"
+        }
+    }
